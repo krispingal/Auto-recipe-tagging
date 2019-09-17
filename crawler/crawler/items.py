cf893d1179fc3ba387fe7b176cde7a2c0083ada3
Krishna Babuji <krishna.pingal@gmail.com>
2019-09-17 08:20:02 -0700

    Add item and item loader
    Added item loader and item to parse recipe

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -6,9 +6,19 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import TakeFirst, Compose, MapCompose
 
 
-class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+def clean_prep_step(prep_step: str):
+    return prep_step.strip().replace('\n', '')
+
+def extract_num_servings(servings: str):
+    return servings.split(' ')[0]
+
+class Recipe(scrapy.Item):
+    name = scrapy.Field()
+    servings = scrapy.Field(input_processor=Compose(TakeFirst(), extract_num_servings))
+    ingredients = scrapy.Field()
+    preparation_steps = scrapy.Field(input_processor=MapCompose(clean_prep_step))
+    rating = scrapy.Field(input_processor=TakeFirst())
+    tags = scrapy.Field(input_processor=TakeFirst())
--- crawler/crawler/spiders/recipes_spider.py
+++ crawler/crawler/spiders/recipes_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from crawler.items import Recipe
+from pprint import pprint
 
 #switch for development
 DEV = True
@@ -10,6 +13,10 @@ class RecipesSpider(scrapy.Spider):
         ]
 
     def parse(self, response):
+        """ Parse search pages
+            Iterate over each recipe in the search page and invoke
+            parse_recipe to etract data
+        """
         _, _, page_num = response.url.partition('page=')
         page_num = int(page_num)
 
@@ -26,5 +33,30 @@ class RecipesSpider(scrapy.Spider):
             yield response.follow(next_page, self.parse)
 
     def parse_recipe(self, response):
-        recipe_name = response.url.split("/")[-1]
-        print(f'{response.url},   {recipe_name}')
+        """ Extract data from recipe
+
+        :param response:
+        :return:
+        """
+        r_name = response.url.split("/")[-1]
+        print(r_name)
+        r = ItemLoader(item=Recipe(), response= response)
+        r.add_value('name', r_name)
+        r.add_xpath('servings', '//dd[@class="yield"]/text()')
+        r.add_xpath('ingredients', '//li[@class="ingredient"]/text()')
+        r.add_xpath('preparation_steps', '//li[@class="preparation-step"]/text()')
+        r.add_xpath('rating', '//span[@class="rating"]/text()')
+        r.add_xpath('tags', '//meta[@name="keywords"]/@content')
+        r.load_item()
+        pprint(r)
+        #r.add_xpath()
+        #r.add_xpath()
+        #recipe_name = response.url.split("/")[-1]
+        #servings = response.xpath('//dd[@class="yield"]/text()').get().split(' ')[0]
+        #ingredients = response.xpath('//li[@class="ingredient"]/text()').getall()
+        #preparation_steps = self.clean_prep_step(response.xpath('//li[@class="preparation-step"]/text()').getall())
+        #rating = response.xpath('//span[@class="rating"]/text()').get()
+        #tags = response.xpath('//meta[@name="keywords"]/@content').get()
+
+        #print(f'{response.url},   {recipe_name}')
+
