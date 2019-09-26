7466eea8721dbbfc50f22d80b356c9721300977b
Krishna Babuji <krishna.pingal@gmail.com>
2019-09-25 20:22:59 -0700

    Add pre-processing
    Extracted recipe and most common tags

--- tag_generation/preprocesing.py
+++ tag_generation/preprocesing.py
@@ -1,14 +1,47 @@
 """Preprocessing for tag generation"""
 
-import pandas as pd
 from tag_generation.util import utils
+import jsonlines
+import csv
 
-def preprocessing(file_loc):
-    df = pd.read_json(file_loc, lines=True)
-    
+
+def get_recipe_tags(file_name: str,
+                    common_tags: list):
+    """Get a dict with preparation steps and tags"""
+    with jsonlines.open(file_name) as reader:
+        for recipe in reader:
+            if 'preparation_steps' not in recipe:
+                print(f"Skipped {recipe['name']}")
+                continue
+            target = {'preparation_steps': ''.join(recipe['preparation_steps'][0])}
+            if 'tags' not in recipe:
+                target.update({tag: 0 for tag in common_tags})
+            else:
+                for tag in common_tags:
+                    target[tag] = 1 if tag in recipe['tags'] else 0
+            yield target
+
+
+def create_tag_gen_file(file_name: str,
+                        common_tags: list,
+                        target_file):
+    with open(target_file, 'w') as csvfile:
+        fieldnames = ['preparation_steps'] + common_tags
+        recipewriter = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_NONNUMERIC)
+        recipewriter.writeheader()
+        recipe_tags = get_recipe_tags(file_name, common_tags)
+        for row in recipe_tags:
+            recipewriter.writerow(row)
+
+
 if __name__ == '__main__':
-    file_loc = '/home/krispin/data/improved-happiness/recipes_bkup.jl'
+    #For test
+    #file_loc = '/home/krispin/data/improved-happiness/'
+    #For dev
+    file_loc = '/home/krispin/data/improved-happiness/proto/'
+    file_name = file_loc + 'recipes.jl'
     k = 6
-    common_tags = utils.get_k_most_common_tags(k, file_loc)
-    print(common_tags)
-
+    _tags = utils.get_k_most_common_tags(k, file_name)
+    _tags = [x for (x, c) in _tags]
+    trg_filename = file_loc + 'recipe_tags.csv'
+    create_tag_gen_file(file_name, _tags, trg_filename)
--- tag_generation/util/utils.py
+++ tag_generation/util/utils.py
@@ -1,18 +1,15 @@
 """"Common functions"""
 import jsonlines
 from collections import Counter
-from pprint import pprint
+#from pprint import pprint
 
 def get_k_most_common_tags(k, file_loc):
     recipes = get_recipe(file_loc=file_loc)
     tag_counter = Counter()
-    for idx, recipe in enumerate(recipes):
-        try:
+    for recipe in recipes:
+        if 'tags' in recipe:
             tags = recipe['tags'][0].split(',')
             tag_counter.update(tags)
-        except:
-            pprint(recipe)
-            break
     return tag_counter.most_common(k)
 
 def get_recipe(file_loc):
